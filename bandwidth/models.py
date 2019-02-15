2ed5482af59a80b33b7644c06bfd485a85cfc6ae
zavx0z <zavx0z@ya.ru>
2019-02-15 19:05:06 +0300

    , blank=True, null=True

--- bandwidth/models.py
+++ bandwidth/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Sms(models.Model):
-    text = models.TextField('текст сообщения', max_length=255)
-    eventType = models.TextField('тип события', max_length=50)
-    direction = models.TextField('направление', max_length=5)
-    _from = models.TextField('от', max_length=12)
-    to = models.TextField('в', max_length=12)
-    time = models.DateTimeField('время')
-    state = models.TextField('состояние', max_length=20)
+    text = models.TextField('текст сообщения', max_length=255, blank=True, null=True)
+    eventType = models.TextField('тип события', max_length=50, blank=True, null=True)
+    direction = models.TextField('направление', max_length=5, blank=True, null=True)
+    _from = models.TextField('от', max_length=12, blank=True, null=True)
+    to = models.TextField('в', max_length=12, blank=True, null=True)
+    time = models.DateTimeField('время', blank=True, null=True)
+    state = models.TextField('состояние', max_length=20, blank=True, null=True)
 
