542aa3ee769a3b9dd45a45ce0f72a363bb55677c
zavx0z <zavx0z@ya.ru>
2019-02-15 04:58:03 +0300

    fix

--- server_sms/settings.py
+++ server_sms/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import django_heroku
-django_heroku.settings(locals())
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -130,4 +130,5 @@ STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
 ]
 # location where django collect all static files
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+django_heroku.settings(locals())
