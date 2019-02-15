e090ade9144c6f238875dc38f6bfffa164230cd3
zavx0z <zavx0z@ya.ru>
2019-02-15 18:48:17 +0300

    fields add

--- bandwidth/models.py
+++ bandwidth/models.py
@@ -2,4 +2,11 @@ from django.db import models
 
 
 class Sms(models.Model):
-    text = models.TextField(max_length=255)
+    text = models.TextField('текст сообщения', max_length=255)
+    eventType = models.TextField('тип события', max_length=50)
+    direction = models.TextField('направление', max_length=5)
+    _from = models.TextField('от', max_length=12)
+    to = models.TextField('в', max_length=12)
+    time = models.DateTimeField('время')
+    state = models.TextField('состояние', max_length=20)
+
