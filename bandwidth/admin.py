602da806bc0b352a30525e33487f0370c3f03c4a
zavx0z <zavx0z@ya.ru>
2019-02-15 21:33:32 +0300

    запрет на добавление

--- bandwidth/admin.py
+++ bandwidth/admin.py
@@ -3,13 +3,9 @@ from bandwidth.models import Sms
 
 
 class SmsAdmin(admin.ModelAdmin):
-    list_display = ('_from', 'to', 'direction', 'time', 'state')
+    list_display = ('_from', 'to', 'direction', 'time', 'state', 'text')
 
     def has_add_permission(self, request, obj=None):
         return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
-
 admin.site.register(Sms, SmsAdmin)
-
